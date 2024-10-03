cd47a132539382dde659f04cab776ca27c9f6371
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-03 11:40:14 +0900

    Got it!

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -19,13 +19,37 @@ impl Shape {
             _ => Err("Shape code does not correspond to a shape"),
         }
     }
+    fn get_shape_score(&self) -> u32 {
+        match &self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
 }
 
-fn get_shape_score(my_shape: &Shape) -> u32 {
-    match my_shape {
-        Shape::Rock => 1,
-        Shape::Paper => 2,
-        Shape::Scissors => 3,
+#[derive(PartialEq, Debug)]
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl Outcome {
+    fn new(desired_outcome_str: &str) -> Result<Outcome, &'static str> {
+        match desired_outcome_str {
+            "X" => Ok(Self::Lose),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => Err("Outcome code does not correspond to an outcome"),
+        }
+    }
+    fn get_score(&self) -> u32 {
+        match &self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Lose => 0,
+        }
     }
 }
 
@@ -61,34 +85,53 @@ fn get_outcome_score(parsed_shapes: &(Shape, Shape)) -> Result<u32, &'static str
     }
 }
 
-pub fn get_round_score(round_shapes: &str) -> u32 {
-    let parsed_shapes = parse_round(round_shapes);
-    get_total_score(parsed_shapes).2
+fn get_correct_shape(opponent_shape: &Shape, desired_outcome: &Outcome) -> Shape {
+    match opponent_shape {
+        Shape::Rock => match desired_outcome {
+            Outcome::Win => Shape::Paper,
+            Outcome::Draw => Shape::Rock,
+            Outcome::Lose => Shape::Scissors,
+        },
+        Shape::Paper => match desired_outcome {
+            Outcome::Win => Shape::Scissors,
+            Outcome::Draw => Shape::Paper,
+            Outcome::Lose => Shape::Rock,
+        },
+        Shape::Scissors => match desired_outcome {
+            Outcome::Win => Shape::Rock,
+            Outcome::Draw => Shape::Scissors,
+            Outcome::Lose => Shape::Paper,
+        },
+    }
 }
 
-fn parse_round(round_shapes: &str) -> (Shape, Shape) {
+fn parse_round(round_shapes: &str) -> (Shape, Shape, Outcome) {
     let mut split_strings = round_shapes.split_whitespace();
-    let opponent = split_strings.next().unwrap();
-    let mine = split_strings.next().unwrap();
+    let opponent_str = split_strings.next().unwrap();
+    let desired_outcome_str = split_strings.next().unwrap();
 
-    let opponent_shape = Shape::new(opponent).unwrap_or_else(|err| {
-        eprintln!("Problem parsing the arguments: {err} {opponent}");
+    let opponent_shape = Shape::new(opponent_str).unwrap_or_else(|err| {
+        eprintln!("Problem parsing the arguments: {err} {opponent_str}");
         process::exit(1);
     });
-    let my_shape = Shape::new(mine).unwrap_or_else(|err| {
-        eprintln!("Problem parsing the arguments: {err} {mine}");
+
+    let desired_outcome = Outcome::new(desired_outcome_str).unwrap_or_else(|err| {
+        eprintln!("Problem parsing the arguments: {err} {desired_outcome_str}");
         process::exit(1);
     });
-    (opponent_shape, my_shape)
+    let my_shape = get_correct_shape(&opponent_shape, &desired_outcome);
+    (opponent_shape, my_shape, desired_outcome)
 }
 
-fn get_total_score(parsed_shapes: (Shape, Shape)) -> (u32, u32, u32) {
-    let shape_score = get_shape_score(&parsed_shapes.1);
-    let outcome_score = get_outcome_score(&parsed_shapes).unwrap_or_else(|err| {
-        eprintln!("Problem getting the outcome score: {err}");
-        process::exit(1);
-    });
+fn get_total_score(parsed_shapes: (Shape, Shape, Outcome)) -> (u32, u32, u32) {
+    let shape_score = parsed_shapes.1.get_shape_score();
+    let outcome_score = parsed_shapes.2.get_score();
     (shape_score, outcome_score, shape_score + outcome_score)
+}
+
+fn get_round_score(round_shapes: &str) -> u32 {
+    let parsed_round = parse_round(round_shapes);
+    get_total_score(parsed_round).2
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
@@ -110,7 +153,7 @@ mod tests {
     fn test_get_round_score() {
         let line = "A Y";
         let total_score = get_round_score(line);
-        assert_eq!(total_score, 8);
+        assert_eq!(total_score, 4);
     }
 
     #[test]
@@ -118,16 +161,16 @@ mod tests {
         let line = "A Y";
         let parsed_shapes = parse_round(line);
         assert_eq!(parsed_shapes.0, Shape::Rock);
-        assert_eq!(parsed_shapes.1, Shape::Paper);
+        assert_eq!(parsed_shapes.2, Outcome::Draw);
 
         let line = "B X";
         let parsed_shapes = parse_round(line);
         assert_eq!(parsed_shapes.0, Shape::Paper);
-        assert_eq!(parsed_shapes.1, Shape::Rock);
+        assert_eq!(parsed_shapes.2, Outcome::Lose);
 
-        let line = "C X";
+        let line = "C Z";
         let parsed_shapes = parse_round(line);
         assert_eq!(parsed_shapes.0, Shape::Scissors);
-        assert_eq!(parsed_shapes.1, Shape::Rock);
+        assert_eq!(parsed_shapes.2, Outcome::Win);
     }
 }

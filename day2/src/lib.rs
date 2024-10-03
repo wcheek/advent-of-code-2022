3c5cc57da6c43cd15844058cf481d8ed81385787
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-03 11:14:11 +0900

    Nice, day2 part 1

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, process};
 
 #[derive(PartialEq, Debug)]
 enum Shape {
@@ -21,11 +21,11 @@ impl Shape {
     }
 }
 
-fn get_shape_score(my_shape: &Shape) -> Result<u32, &'static str> {
+fn get_shape_score(my_shape: &Shape) -> u32 {
     match my_shape {
-        Shape::Rock => Ok(1),
-        Shape::Paper => Ok(2),
-        Shape::Scissors => Ok(3),
+        Shape::Rock => 1,
+        Shape::Paper => 2,
+        Shape::Scissors => 3,
     }
 }
 
@@ -51,46 +51,51 @@ fn get_outcome_score(parsed_shapes: &(Shape, Shape)) -> Result<u32, &'static str
     let lose_scenario = paper_loses_rock || rock_loses_scissors || scissors_loses_paper;
 
     if win_scenario {
-        Ok(0)
+        Ok(6)
     } else if draw_scenario {
         Ok(3)
     } else if lose_scenario {
-        Ok(6)
+        Ok(0)
     } else {
         Err("Missing scenario")
     }
 }
 
-pub fn get_round_score(round_shapes: &str) -> Result<u32, &'static str> {
-    // TODO Parse round
-    if let Ok(parsed_shapes) = parse_round(round_shapes) {
-        if let Ok(scores) = get_total_score(parsed_shapes) {
-            Ok(scores.2)
-        } else {
-            Err("Something went wrong getting total score")
-        }
-    } else {
-        eprint!("Bad shapes: {round_shapes}");
-        Err("Something went wrong parsing round")
-    }
+pub fn get_round_score(round_shapes: &str) -> u32 {
+    let parsed_shapes = parse_round(round_shapes);
+    get_total_score(parsed_shapes).2
 }
 
-fn parse_round(round_shapes: &str) -> Result<(Shape, Shape), &'static str> {
-    let (opponent, mine) = round_shapes.split_at(1);
-    Ok((Shape::new(opponent)?, Shape::new(mine)?))
+fn parse_round(round_shapes: &str) -> (Shape, Shape) {
+    let mut split_strings = round_shapes.split_whitespace();
+    let opponent = split_strings.next().unwrap();
+    let mine = split_strings.next().unwrap();
+
+    let opponent_shape = Shape::new(opponent).unwrap_or_else(|err| {
+        eprintln!("Problem parsing the arguments: {err} {opponent}");
+        process::exit(1);
+    });
+    let my_shape = Shape::new(mine).unwrap_or_else(|err| {
+        eprintln!("Problem parsing the arguments: {err} {mine}");
+        process::exit(1);
+    });
+    (opponent_shape, my_shape)
 }
 
-fn get_total_score(parsed_shapes: (Shape, Shape)) -> Result<(u32, u32, u32), &'static str> {
-    let shape_score = get_shape_score(&parsed_shapes.1)?;
-    let outcome_score = get_outcome_score(&parsed_shapes)?;
-    Ok((shape_score, outcome_score, shape_score + outcome_score))
+fn get_total_score(parsed_shapes: (Shape, Shape)) -> (u32, u32, u32) {
+    let shape_score = get_shape_score(&parsed_shapes.1);
+    let outcome_score = get_outcome_score(&parsed_shapes).unwrap_or_else(|err| {
+        eprintln!("Problem getting the outcome score: {err}");
+        process::exit(1);
+    });
+    (shape_score, outcome_score, shape_score + outcome_score)
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let mut total_score = 0;
     if let Ok(contents) = fs::read_to_string("input.txt") {
         for line in contents.lines() {
-            total_score += get_round_score(line)?;
+            total_score += get_round_score(line);
         }
     }
     println!("{total_score}");
@@ -104,28 +109,25 @@ mod tests {
     #[test]
     fn test_get_round_score() {
         let line = "A Y";
-        if let Ok(total_score) = get_round_score(line) {
-            assert_eq!(total_score, 8);
-        }
+        let total_score = get_round_score(line);
+        assert_eq!(total_score, 8);
     }
 
     #[test]
     fn test_parse_round() {
         let line = "A Y";
-        if let Ok(parsed_shapes) = parse_round(line) {
-            assert_eq!(parsed_shapes.0, Shape::Rock);
-            assert_eq!(parsed_shapes.1, Shape::Paper);
-        }
+        let parsed_shapes = parse_round(line);
+        assert_eq!(parsed_shapes.0, Shape::Rock);
+        assert_eq!(parsed_shapes.1, Shape::Paper);
+
         let line = "B X";
-        if let Ok(parsed_shapes) = parse_round(line) {
-            assert_eq!(parsed_shapes.0, Shape::Paper);
-            assert_eq!(parsed_shapes.1, Shape::Rock);
-        }
+        let parsed_shapes = parse_round(line);
+        assert_eq!(parsed_shapes.0, Shape::Paper);
+        assert_eq!(parsed_shapes.1, Shape::Rock);
+
         let line = "C X";
-        if let Ok(parsed_shapes) = parse_round(line) {
-            dbg!("{line}");
-            assert_eq!(parsed_shapes.0, Shape::Scissors);
-            assert_eq!(parsed_shapes.1, Shape::Scissors);
-        }
+        let parsed_shapes = parse_round(line);
+        assert_eq!(parsed_shapes.0, Shape::Scissors);
+        assert_eq!(parsed_shapes.1, Shape::Rock);
     }
 }

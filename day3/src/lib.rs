3c429354295c822bcf97be28596b1283e2a511ca
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-04 10:05:52 +0900

    Nice! day3 completed.

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -4,16 +4,12 @@ fn find_matching_characters<'a>(string1: &'a str, string2: &'a str) -> Vec<Strin
     let mut matching_chars = vec![];
     for char1 in string1.chars() {
         for char2 in string2.chars() {
-            if char1 == char2 {
+            if char1 == char2 && !matching_chars.contains(&char1.to_string()) {
                 matching_chars.push(char1.to_string().to_owned())
             };
         }
     }
     matching_chars
-}
-
-fn split_string(line: &str) -> (&str, &str) {
-    line.split_at(line.len() / 2)
 }
 
 fn get_scores(chars: Vec<String>) -> Vec<usize> {
@@ -32,11 +28,13 @@ fn get_scores(chars: Vec<String>) -> Vec<usize> {
 pub fn run() -> Result<(), Box<dyn Error>> {
     if let Ok(contents) = fs::read_to_string("input.txt") {
         let mut total_score = 0;
-        for line in contents.lines() {
-            let (compartment_1, compartment_2) = split_string(line);
-            let matching_chars = find_matching_characters(compartment_1, compartment_2);
-            let scores = get_scores(matching_chars);
-            total_score += scores[0];
+        let mut lines = contents.lines();
+        while let Some(elf1) = lines.next() {
+            let elf2 = lines.next().expect("Should not panic");
+            let elf3 = lines.next().expect("Should not panic");
+            let matching_chars = find_matching_characters(elf1, elf2);
+            let matching_chars = find_matching_characters(&matching_chars.join(""), elf3);
+            total_score += get_scores(matching_chars)[0];
         }
         println!("{total_score}");
     }
@@ -54,44 +52,26 @@ mod tests {
         ("ttgJtRGJQctTZtZT", "t"),
         ("CrZsJsPPZsGzwwsLwLmpwMDw", "s"),
     ];
-    fn test_parse_row() {
-        let test_case = ("vJrwpWtwJgWrhcsFMMfFFhFp", "p");
-    }
-
-    #[test]
-    fn test_split_string() {
-        let test_case = ("vJrwpWtwJgWrhcsFMMfFFhFp", "p");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        assert_eq!(compartment_1, "vJrwpWtwJgWr");
-        assert_eq!(compartment_2, "hcsFMMfFFhFp");
-
-        let test_case = ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "p");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        assert_eq!(compartment_1, "jqHRNqRjqzjGDLGL");
-        assert_eq!(compartment_2, "rsFMfFZSrLrFZsSL");
-
-        let test_case = ("PmmdzqPrVvPwwTWBwg", "p");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        assert_eq!(compartment_1, "PmmdzqPrV");
-        assert_eq!(compartment_2, "vPwwTWBwg");
-    }
 
     #[test]
     fn test_find_matching_characters() {
-        let test_case = ("vJrwpWtwJgWrhcsFMMfFFhFp", "p");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        let matching_chars = find_matching_characters(compartment_1, compartment_2);
-        assert_eq!(matching_chars[0], test_case.1);
+        let test_case1 = ("vJrwpWtwJgWrhcsFMMfFFhFp", "p");
+        let test_case2 = ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "L");
+        let test_case3 = ("PmmdzqPrVvPwwTWBwg", "p");
+        let matching_chars = find_matching_characters(test_case1.0, test_case2.0);
+        let matching_chars = find_matching_characters(&matching_chars.join(""), test_case3.0);
+        println!("{:?}", matching_chars.join(""));
+        // assert_eq!(matching_chars[0],);
 
-        let test_case = ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "L");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        let matching_chars = find_matching_characters(compartment_1, compartment_2);
-        assert_eq!(matching_chars[0], test_case.1);
-
-        let test_case = ("PmmdzqPrVvPwwTWBwg", "P");
-        let (compartment_1, compartment_2) = split_string(test_case.0);
-        let matching_chars = find_matching_characters(compartment_1, compartment_2);
-        assert_eq!(matching_chars[0], test_case.1);
+        // let test_case = ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "L");
+        // let (compartment_1, compartment_2) = split_string(test_case.0);
+        // let matching_chars = find_matching_characters(compartment_1, compartment_2);
+        // assert_eq!(matching_chars[0], test_case.1);
+        //
+        // let test_case = ("PmmdzqPrVvPwwTWBwg", "P");
+        // let (compartment_1, compartment_2) = split_string(test_case.0);
+        // let matching_chars = find_matching_characters(compartment_1, compartment_2);
+        // assert_eq!(matching_chars[0], test_case.1);
     }
 
     #[test]

b5fd54ee417639afb4aca71109be3ce9309eafe3
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-10 09:56:18 +0900

    Finally this is working

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,32 +1,31 @@
 use regex::Regex;
-use std::{array::from_fn, error::Error, fs};
+use std::{error::Error, fs};
 
 mod stack;
 
-fn parse_layout(initial_layout: &str) -> [stack::Stack<&str>; 9] {
-    let mut empty_layout: [stack::Stack<&str>; 9] = from_fn(|x| stack::Stack::build(vec![]));
+fn parse_layout(initial_layout: &str) -> Vec<stack::Stack<&str>> {
+    let mut empty_layout: Vec<stack::Stack<&str>> = vec![stack::Stack::new(); 9];
     let re = Regex::new(r"(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})\s(\[\w\]|\s{3})").unwrap();
 
     let lines = initial_layout.lines();
     for line in lines {
-        // let caps_enum = re.captures_iter(line)..enumerate();
-        // for (ind, cap) in caps_enum {
-        // }
         if let Some(caps) = re.captures(line) {
             let caps_enum = caps.iter().enumerate();
             for (ind, cap) in caps_enum {
-                if ind > 0 {
-                    if let Some(cap) = cap {
-                        if cap.as_str() != "   " {
-                            empty_layout[ind - 1].push(cap.as_str());
-                        };
-                    }
+                if let Some(cap) = cap {
+                    if cap.as_str() != "   " {
+                        let val = cap.as_str().trim_start_matches("[").trim_end_matches("]");
+                        if ind > 0 {
+                            empty_layout[ind - 1].push(val);
+                        }
+                    };
                 }
             }
         };
     }
     dbg!(empty_layout)
 }
+
 pub fn run() -> Result<(), Box<dyn Error>> {
     if let Ok(contents) = fs::read_to_string("input.txt") {
         let (initial_layout, instructions) = contents.split_once("\r\n\r").unwrap();
@@ -41,10 +40,8 @@ mod tests {
 
     #[test]
     fn test_parse_initial_layout() {
-        let initial_layout = "[V]         [T]         [J]        \r\n[Q]         [M] [P]     [Q]     [J]\r\n[W] [B]     [N] [Q]     [C]     [T]\r\n[
-M] [C]     [F] [N]     [G] [W] [G]\r\n[B] [W] [J] [H] [L]     [R] [B] [C]\r\n[N] [R] [R] [W] [W] [W] [D] [N] [F]\r\n[Z] [Z] [Q] [S] [F] [P] [B] [Q] [L]\r\
-n[C] [H] [F] [Z] [G] [L] [V] [Z] [H]\r\n 1   2   3   4   5   6   7   8   9 ";
-        let expected = [
+        let initial_layout = "[V]         [T]         [J]        \r\n[Q]         [M] [P]     [Q]     [J]\r\n[W] [B]     [N] [Q]     [C]     [T]\r\n[M] [C]     [F] [N]     [G] [W] [G]\r\n[B] [W] [J] [H] [L]     [R] [B] [C]\r\n[N] [R] [R] [W] [W] [W] [D] [N] [F]\r\n[Z] [Z] [Q] [S] [F] [P] [B] [Q] [L]\r\n[C] [H] [F] [Z] [G] [L] [V] [Z] [H]\r\n 1   2   3   4   5   6   7   8   9 ";
+        let expected = vec![
             stack::Stack::build(vec!["V", "Q", "W", "M", "B", "N", "Z", "C"]),
             stack::Stack::build(vec!["B", "C", "W", "R", "Z", "H"]),
             stack::Stack::build(vec!["J", "R", "Q", "F"]),
--- day5/src/stack.rs
+++ day5/src/stack.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Stack<T> {
     stack: Vec<T>,
 }

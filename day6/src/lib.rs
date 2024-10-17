e429a0eda6c52c16fd8ad4f88fa8184b8d1ffac6
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-17 15:13:38 +0900

    Got part 1 on first try!

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,8 +1,25 @@
-use std::{error::Error, fs};
+use std::{collections::HashSet, error::Error, fs, hash::Hash};
+
+fn has_unique_elements<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Eq + Hash,
+{
+    let mut uniq = HashSet::new();
+    iter.into_iter().all(move |x| uniq.insert(x))
+}
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     if let Ok(contents) = fs::read_to_string("input.txt") {
-        dbg!(contents);
+        let mut current = ['0'; 4];
+        for (ind, c) in contents.char_indices() {
+            current[ind % 4] = c;
+            if has_unique_elements(current) && !current.contains(&'0') {
+                dbg!(&current);
+                dbg!(&ind + 1);
+                break;
+            }
+        }
     } else {
         panic!("Contents could not be read");
     }

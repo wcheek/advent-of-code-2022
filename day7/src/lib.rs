42154b67f68ac19b91c187b82a6b091ecd923ecf
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-10-22 16:12:43 +0900

    Starting to parse

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,3 +1,25 @@
+use std::{error::Error, fs, str::Split};
+
+fn process_command(split_strings: &[&str]) {}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
+    if let Ok(contents) = fs::read_to_string("input.txt") {
+        let split_words: Vec<&str> = contents.split(' ').collect();
+        if split_words[0] == "$" {
+            process_command(&split_words[1..split_words.len() - 1]);
+        }
+    } else {
+        panic!("Contents could not be read");
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_command() {
+        let cd = "$ cd /";
+    }
 }
